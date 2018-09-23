d779381db1e3c4e227a935d19b1735bd901deaff
Gus Monaco <gustavo.monaco@dasi.com>
2018-09-23 12:48:16 -0400

    Stone marker, ball and hoops by Andrew

---
+++ Augmented Hoops (1)/Assets/Game Scripts/DestroyBall.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyBall : MonoBehaviour {
+
+    private int numOfBounces = 0;
+
+    private void OnCollisionEnter(Collision collision) {
+
+        if (collision.gameObject.name == "Surface") {
+            numOfBounces++;
+            if (numOfBounces >= 3)
+                Destroy(gameObject);
+        }
+        
+    }
+
+
+
+}
---
+++ Augmented Hoops (1)/Assets/Test Cases/BounceCount.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Note: 
+// Counts the number of times ball hits ground. 
+// If count > 3 issue warning for test case. 
+
+public class BounceCount : MonoBehaviour {
+
+    // Counter
+    private int bounceCount = 0;
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.name == "Surface") {
+            bounceCount++;
+            Debug.Log("Ball made contact with ground " + bounceCount + " times");
+
+            // Issues warning to console if ball bounces more than 3x
+            if (bounceCount > 3)
+                Debug.LogWarning("Test Case 'BounceCount.cs' failed!");
+        }
+
+    }
+
+
+}
